35914505cbbef0a373b6671e07d5490fb9b32a1f
Igor Srdoč <igor@srdoc.si>
2022-04-12 03:54:34 +0200

    Fix server inverse search results

--- server/app.py
+++ server/app.py
@@ -12,11 +12,11 @@ def search_handler():
     # simulate long request
     time.sleep(2)
 
-    search_text = request.args.get('q')
-    if not search_text:
+    event_type = request.args.get('type')
+    if not event_type:
         return jsonify(mock_data)
-    filtered = [x for x in mock_data if search_text not in x['type']]
+    filtered = [x for x in mock_data if event_type.lower() in x['type'].lower()]
     return jsonify(filtered)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port='5001')
